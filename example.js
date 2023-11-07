14fcbe92a9fe32648eba971a4a549e47254f9234
mavci <mavci@aligntech.com>
2023-11-07 16:26:26 +0100

    third

--- example.js
+++ example.js
@@ -2,11 +2,12 @@ const puppeteer = require("puppeteer");
 const devices = require("puppeteer/DeviceDescriptors");
 
 (async () => {
-  //emulate devices
+  //faking geolocation
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.emulate(devices["iPhone X"]);
-  await page.goto("https://www.pptr.dev");
-  await page.waitFor(5000);
+
+  //Grant rmission for geolocation changes
+  const context = browser.defaultBrowserContext();
+  await context.overridePermissions("https://pptr.dev", ["geolocation"]);
   await browser.close();
 })();
