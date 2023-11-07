3a4f69844d671458061694290fe7867c76f1aa48
mavci <mavci@aligntech.com>
2023-11-07 14:32:25 +0100

    first commit

---
+++ example.js
@@ -0,0 +1,10 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  //convert page to PDF file
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://wwww.example.com", { waitUntil: "networkidle0" });
+  await page.pdf({ path: "example.pdf", format: "A4" });
+  await browser.close();
+})();
